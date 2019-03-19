412c94b8c9e361e8a5035504a36dc5a3dc95f338
Frank Ang <frankang@amazon.com>
2019-03-19 14:12:58 +0800

    set cloudwatch logs to 30-day retention

---
+++ miscellaneous/set-cw-log-retention.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -r retention="30"
+for L in $(aws logs describe-log-groups \
+    --query 'logGroups[?!not_null(retentionInDays)] | [].logGroupName' \
+    --output text)
+do
+   echo setting retention for log-group-name:${L} to retention-in-days:${retention}
+   aws logs  put-retention-policy --log-group-name ${L} \
+   --retention-in-days ${retention}
+done
